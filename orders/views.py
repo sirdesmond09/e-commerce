b1e89756aaf36952050e99262b2aabba41eda2d6
sirdesmond09 <sirdesmond09@gmail.com>
2020-05-30 23:01:21 +0100

    allowed checkout

---
+++ orders/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderForm
+from cart.cart import Cart
+
+def create_order(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                #fill in the items in the cart into the db
+                OrderItem.objects.create(order = order, product=item['product'], price = item['price'], quantity=item['quantity'] )
+            #clear the cart
+            cart.clear()
+            context = {
+                'order' : order
+            }
+            return render(request, 'orders/order/created.html', context)
+    else:
+        form = OrderForm()
+    context = {
+        'cart': cart, 
+        'form': form
+    }
+
+    return render(request,'orders/order/create.html', context)
